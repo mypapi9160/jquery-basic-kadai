3cc064fd8fcd1ec440000aa9037a26979344392b
mypapi9160 <158793809+mypapi9160@users.noreply.github.com>
2024-04-24 07:47:23 +0900

    kadai_004をアップロード

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,13 +1,9 @@
-$(function() {
-  $('window').on({
-    'load': () => {
+    $(window).on('load', function (){
       //コンソールに「loadイベントが発生しました」と出力
       console.log('loadイベントが発生しました');
-    },
+    });
 
-    'scroll': () => {
+    $(document).on('scroll', function (){
       //コンソールに「scrollイベントが発生しました」と出力する
       console.log('scrollイベントが発生しました');
-    }
-  });
-});
+    });
